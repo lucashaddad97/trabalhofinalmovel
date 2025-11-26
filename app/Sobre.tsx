d49344ba50292203a3649a71d6946516ba98faf7
Lucas haddad <lucashaddad.97@hotmail.com>
2025-11-26 13:15:38 -0300

    addpag Minhacarteira

--- app/Sobre.tsx
+++ app/Sobre.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function MinhaCarteiraSobre() {
   return (
